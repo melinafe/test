c0120f6674026f8d89bf5ce0ec7a8eabdc9eb989
melinafe <148732733+melinafe@users.noreply.github.com>
2024-01-18 21:44:37 +0700

    Add files via upload

---
+++ twentythree/home.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Twenty Four</title>
+    <link rel="stylesheet" types="text/css" href="css/style.css">
+    <link rel="stylesheet" types="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+</head>
+<body>
+
+    <!-- header -->
+    <div class="medsos">
+        <div class="container">
+            <ul>
+                <li><a href="#"><i class="fa-brands fa-instagram"></i></i></a></li>
+                <li><a href="#"><i class="fa-brands fa-twitter"></i></i></a></li>
+            </ul>
+        </div>
+    </div>
+    <header>
+        <div class="container">
+            <h1><a href="index.html">Twenty Four</a></h1>
+            <ul>
+                <li class="active"><a href="index.html">HOME</a></li>
+                <li><a href="product.html">PRODUCT</a></li>
+                <li><a href="contact.html">CONTACT</a></li>
+               
+            </ul>
+        </div>
+    </header>
+    
+ 	<!-- banner -->
+     <section class="banner">
+		<h2>Find Your Style Now!</h2>
+	</section>
+
+	<!-- about -->
+	<section class="about">
+		<div class="container">
+			<h3>Info Toko</h3>
+			<p> Twenty Four menjual berbagai macam fashion yang bekerja sama dengan brand-brand lokal di Indonesia
+                sebagai bentuk dukungan untuk usaha kecil dan menengah dalam mempromosikan dan menjual produknya.
+                Semua produk yang dapat dipesan adalah produk ready stock! Produk yang dipesan sebelum jam 14.00 akan dikirim dihari yang sama.
+             </p>
+		</div>
+	</section>
+
+	<!-- footer -->
+	<footer>
+		<div class="container">
+			<small> Enjoy your shopping with <strong>TWENTY FOUR</strong> </small>
+		</div>
+	</footer>
+</body>
+</html>
